592a36f27bf5f090966a28911ae7a99c5adc1920
Naoyuki Yamada <yamada@kirishikistudios.com>
2017-01-15 01:11:54 +0000

    ファイル構成変更

---
+++ application/views/about.php
@@ -0,0 +1,112 @@
+
+	<header id="fh5co-header" class="fh5co-cover fh5co-cover-sm" role="banner" style="background-image:url(<?=base_url();?>images/img_bg_11.jpg);">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeIn">
+							<h1>私たちについて</h1>
+							<h2>いっしょにこの場所をつくる、仲間を募集しています</a></h2>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	
+	<div id="fh5co-about-section">
+		<div class="container">
+			<div class="about-content">
+				<div class="row animate-box">
+					<div class="col-md-6">
+						<img class="img-responsive" src="<?=base_url();?>images/img_bg_11.jpg" alt="about">
+					</div>
+					<div class="col-md-6">
+						<div class="desc">
+							<h3>きっかけ</h3>
+							<p>それは2017年1月に行われたイベントがきっかけでした。</p> 
+							<p>「小高ハッカソン！」と名付けられたそのイベントで、学生や社会人、小高の人、東京の人、老若男女入り乱れて議論する中で、この場所のコンセプトが浮かび上がってきました。</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Meet Our Staff</h2>
+					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4 col-sm-4 animate-box" data-animate-effect="fadeIn">
+					<div class="fh5co-staff">
+						<img src="<?=base_url();?>images/person1.jpg" alt="Free HTML5 Templates by FreeHTML5.co">
+						<h3>Jean Smith</h3>
+						<strong class="role">Web Designer</strong>
+						<p>Quos quia provident consequuntur culpa facere ratione maxime commodi voluptates id repellat velit eaque aspernatur expedita. Possimus itaque adipisci.</p>
+						<ul class="fh5co-social-icons">
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-github"></i></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-4 animate-box" data-animate-effect="fadeIn">
+					<div class="fh5co-staff">
+						<img src="<?=base_url();?>images/person2.jpg" alt="Free HTML5 Templates by FreeHTML5.co">
+						<h3>Hush Raven</h3>
+						<strong class="role">Front-end Developer</strong>
+						<p>Quos quia provident consequuntur culpa facere ratione maxime commodi voluptates id repellat velit eaque aspernatur expedita. Possimus itaque adipisci.</p>
+						<ul class="fh5co-social-icons">
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-github"></i></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-4 animate-box" data-animate-effect="fadeIn">
+					<div class="fh5co-staff">
+						<img src="<?=base_url();?>images/person3.jpg" alt="Free HTML5 Templates by FreeHTML5.co">
+						<h3>Alex King</h3>
+						<strong class="role">Back-end Developer</strong>
+						<p>Quos quia provident consequuntur culpa facere ratione maxime commodi voluptates id repellat velit eaque aspernatur expedita. Possimus itaque adipisci.</p>
+						<ul class="fh5co-social-icons">
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-github"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-started">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Newsletter</h2>
+					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+				</div>
+			</div>
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2">
+					<form class="form-inline">
+						<div class="col-md-6 col-sm-6">
+							<div class="form-group">
+								<label for="email" class="sr-only">Email</label>
+								<input type="email" class="form-control" id="email" placeholder="Email">
+							</div>
+						</div>
+						<div class="col-md-6 col-sm-6">
+							<button type="submit" class="btn btn-default btn-block">Subscribe</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
---
+++ application/views/index.php
@@ -0,0 +1,278 @@
+	<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(<?=base_url();?>images/kominka_01.jpg);">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10 col-md-offset-1 text-center">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeIn">
+							<h1>ぼくらの場所をつくる。</h1>
+							<h2>空き家をリノベーションして作る、学生向けカフェ兼自習室</h2>
+							<p><a href="#" class="btn btn-default">ご利用案内</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	
+	<div class="container">
+		<div class="row">
+			<div id="fh5co-section">
+				<div class="col-nineth">
+					<div class="row">
+						<div class="col-third">
+							<div class="col-md-12">
+								<span class="icon">
+									<i class="icon-eye"></i>
+								</span>
+								<div class="desc">
+									<h3>カフェ兼自習室。</h3>
+									<p>一人でも、グループでも、立ち寄りやすい場所です。メンバーの顔が分かることで、気兼ねなく立ち寄れる場所にしたいと思っています。</p>
+									<p><a href="#" class="btn btn-primary">写真・動画をみてみる</a></p>
+								</div>
+							</div>
+						</div>
+						<div class="col-third">
+							<div class="col-md-12">
+								<span class="icon">
+									<i class="icon-power"></i>
+								</span>
+								<div class="desc">
+									<h3>すきま時間にふらっと立ち寄れて、予約もできる。</h3>
+									<p>電車の待ち時間に利用することを想定して、短時間の滞在がしやすくしました。じっくり勉強したいときは予約も簡単にできます。もちろんフリーWifiもあります。</p>
+									<p><a href="#" class="btn btn-primary">写真をみる</a></p>
+								</div>
+							</div>
+						</div>
+						<div class="col-third">
+							<div class="col-md-12">
+								<span class="icon">
+									<i class="icon-laptop"></i>
+								</span>
+								<div class="desc">
+									<h3>昼間は別の用途に使って、みんなで運営する。</h3>
+									<p>学生向けにオープンするのは15時から。それまでは大人の交流スペースとして利用します。運営スタッフを絶賛募集中です。</p>
+									<p><a href="#" class="btn btn-primary">運営に参加する</a></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-forth fh5co-video fh5co-bg" style="background-image: url(<?=base_url();?>images/image_1.jpg)">
+					<a href="https://vimeo.com/channels/staffpicks/93951774" class="popup-vimeo"><i class="icon-video2"></i></a>
+						<div class="overlay"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="fh5co-bg-section">
+		<div class="container">
+			<div class="row">
+				<div id="fh5co-features-2">
+					<div class="col-feature-9">
+						<div class="row">
+							<div class="col-md-12 fh5co-heading animate-box">
+								<h2>小高フレンドベースはこんなところ</h2>
+								<p>電車の待ち時間に立ち寄るのに便利な工夫をこらしました。</p>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-7 col-sm-7 col-md-pull-2 col-sm-pull-2 animate-box" data-animate-effect="fadeInRight">
+								<div class="feature-image">
+									<img src="<?=base_url();?>images/work_1.png" alt="work">
+								</div>
+							</div>
+							<div class="col-md-5 col-sm-5">
+								<div class="feature-left animate-box" data-animate-effect="fadeInLeft">
+									<span class="icon">
+										<i class="icon-check"></i>
+									</span>
+									<div class="feature-copy">
+										<h3>フリードリンクと雑誌</h3>
+										<p>セルフサービスのドリンクコーナーと、学生スタッフ厳選の雑誌（ジャンプ、マガジン）が常備されて読み放題。</p>
+									</div>
+								</div>
+
+								<div class="feature-left animate-box" data-animate-effect="fadeInLeft">
+									<span class="icon">
+										<i class="icon-check"></i>
+									</span>
+									<div class="feature-copy">
+										<h3>フリー席はライブカメラでチェック</h3>
+										<p>ちょっと思い立って寄りたいとき、空いてるかな？と思ったらライブカメラで確認。</p>
+									</div>
+								</div>
+
+								<div class="feature-left animate-box" data-animate-effect="fadeInLeft">
+									<span class="icon">
+										<i class="icon-check"></i>
+									</span>
+									<div class="feature-copy">
+										<h3>予約席</h3>
+										<p>じっくり勉強したいときは予約席で。このウェブサイトから簡単に予約できます。</p>
+									</div>
+								</div>
+
+							</div>
+						</div>
+					</div>
+
+					<div class="col-feature-3">
+						<div id="fh5co-counter" class="fh5co-bg fh5co-counter">
+							<div class="row">
+								<div class="display-t">
+									<div class="display-tc">
+										<div class="col-md-12 animate-box">
+											<div class="feature-center">
+												<span class="counter js-counter" data-from="0" data-to="15" data-speed="5000" data-refresh-interval="50">1</span>
+												<span class="counter-label">時オープン</span>
+
+											</div>
+										</div>
+										<div class="col-md-12 animate-box">
+											<div class="feature-center">
+												<span class="counter js-counter" data-from="0" data-to="21" data-speed="5000" data-refresh-interval="50">1</span>
+												<span class="counter-label">時クローズ</span>
+											</div>
+										</div>
+										<div class="col-md-12 animate-box">
+											<div class="feature-center">
+												<span class="counter js-counter" data-from="0" data-to="16" data-speed="5000" data-refresh-interval="50">1</span>
+												<span class="counter-label">予約席</span>
+											</div>
+										</div>
+										<div class="col-md-12 animate-box">
+											<div class="feature-center">
+												<span class="counter js-counter" data-from="0" data-to="30" data-speed="5000" data-refresh-interval="50">1</span>
+												<span class="counter-label">フリー席</span>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div id="fh5co-portfolio">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>昼間の用途</h2>
+					<p>夕方〜夜は学生のカフェですが、昼間は高齢者のお茶会や、子育て家族向けの談話スペースとして利用しています。</p>
+				<div>
+						<img class="img-responsive" src="<?=base_url();?>images/timetable.png" alt="timetable">
+				</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="project">
+					<div class="col-md-8 col-md-push-5 animate-box" data-animate-effect="fadeInLeft">
+						<img class="img-responsive" src="<?=base_url();?>images/work_2.png" alt="teatime">
+					</div>
+					<div class="col-md-4 col-md-pull-8 animate-box" data-animate-effect="fadeInRight">
+						<div class="mt">
+							<h3>大人のお茶会</h3>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. </p>
+							<ul class="list-nav">
+								<li><i class="icon-check"></i>Far far away, behind the word</li>
+								<li><i class="icon-check"></i>There live the blind texts</li>
+								<li><i class="icon-check"></i>Separated they live in bookmarksgrove</li>
+								<li><i class="icon-check"></i>Semantics a large language ocean</li>
+								<li><i class="icon-check"></i>A small river named Duden</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="project">
+					<div class="col-md-8 col-md-pull-1 animate-box" data-animate-effect="fadeInLeft">
+						<img class="img-responsive" src="<?=base_url();?>images/work_1.png" alt="work">
+					</div>
+					<div class="col-md-4 animate-box" data-animate-effect="fadeInRight">
+						<div class="mt">
+							<div>
+								<h4><i class="icon-user"></i>Real Project For Real Solutions</h4>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+							</div>
+							<div>
+								<h4><i class="icon-video2"></i>Real Project For Real Solutions</h4>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+							</div>
+							<div>
+								<h4><i class="icon-shield"></i>Real Project For Real Solutions</h4>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+							</div>
+						</div>
+					</div>
+				</div>	
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-testimonial">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Testimonial</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4 animate-box">
+					<div class="testimony">
+						<blockquote>
+							&ldquo;Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius&rdquo;
+						</blockquote>
+						<p class="author"><cite>John Doe</cite></p>
+					</div>
+				</div>
+				<div class="col-md-4 animate-box">
+					<div class="testimony">
+						<blockquote>
+							&ldquo;Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius&rdquo;
+						</blockquote>
+						<p class="author"><cite>Rob Smith</cite></p>
+					</div>
+				</div>
+				<div class="col-md-4 animate-box">
+					<div class="testimony">
+						<blockquote>
+							&ldquo;Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius&rdquo;
+						</blockquote>
+						<p class="author"><cite>Jane Doe</cite></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<div id="fh5co-started">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Newsletter</h2>
+					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+				</div>
+			</div>
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2">
+					<form class="form-inline">
+						<div class="col-md-6 col-sm-6">
+							<div class="form-group">
+								<label for="email" class="sr-only">Email</label>
+								<input type="email" class="form-control" id="email" placeholder="Email">
+							</div>
+						</div>
+						<div class="col-md-6 col-sm-6">
+							<button type="submit" class="btn btn-default btn-block">Subscribe</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
